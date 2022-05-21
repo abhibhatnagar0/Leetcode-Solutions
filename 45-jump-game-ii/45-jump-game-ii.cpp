c25bebba452f5f5ec57e8a82f86349b063b1ee15
abhibhatnagar0 <77234929+abhibhatnagar0@users.noreply.github.com>
2022-05-21 12:27:10 +0530

    Time: 268 ms (37.32%), Space: 22 MB (5.51%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -5,12 +5,13 @@ public:
             vector<int> dp(10001,-1);
             dp[n-1]=0; // dp[i] stores no of min jumps req from ith index to reach n-1 index
             for(int i=n-2;i>=0;i--){
-                    if(i+nums[i]>=n-1) dp[i]=1;
-                    else{
+                    if(i+nums[i]>=n-1) dp[i]=1; // at ith index, we can go upto i+nums[i]
+                    else{              
+                            //going from i to j in 1 jump and then j to n-1
                             int reach=i+nums[i];
                             int j=i+1;
-                            int minJumps=INT_MAX;
-                            while(j<=reach){
+                            int minJumps=INT_MAX; // stores minJumps from j to n-1
+                            while(j<=reach){  
                                     if(dp[j]!=-1)
                                 minJumps=min(minJumps,dp[j]);
                                     j++;
