4a0f4dad6f2f133fc4c16dbd48d178b77c875303
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-05-26 15:23:26 +0530

    Time: 52 ms (73.10%), Space: 14.7 MB (32.56%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.cpp
@@ -9,34 +9,16 @@
 class Solution {
 public:
     ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
-        ListNode* temp=headA;
-            int sizeA=0;
-            while(temp!=NULL){
-                    sizeA++;
-                    temp=temp->next;
-            }
-            temp=headB;
-             int sizeB=0;
-            while(temp!=NULL){
-                    sizeB++;
-                    temp=temp->next;
-            }
-            if(sizeA==0 || sizeB==0) return NULL;
-            ListNode* p1=headA;
-            ListNode* p2=headB;
-            if(sizeA>sizeB){
-                   int p=sizeA-sizeB;
-                   while(p--) p1=p1->next;
-            }
-            else if(sizeB>sizeA){
-                     int p=sizeB-sizeA;
-                   while(p--) p2=p2->next;
-            }
+        ListNode* p1=headA;
+        ListNode* p2=headB;
+        // making both move equal distance in a circle, intersection gives ans
             while(p1!=p2){
-                    if(p1==NULL || p2==NULL) return NULL;
-                    p1=p1->next;
-                    p2=p2->next;
+                    if(p1==NULL) p1=headB;
+                    else p1=p1->next;
+                    
+                    if(p2==NULL) p2=headA;
+                    else p2=p2->next;
             }
-            return p1; 
+            return p1; //=p2 it can be NULL as well which indicates no intersection
     }
 };
