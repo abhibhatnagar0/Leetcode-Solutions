11aa5b4e30a2ff38db5d5feb344502fd5ccd476b
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-05-29 20:14:58 +0530

    Time: 32 ms (70.53%), Space: 27.1 MB (57.20%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool checkPossibility(vector<int>& nums) {
+        int n=nums.size();
+            int dips=0;
+            int idx; //index of last dip/only dip
+            for(int i=1;i<n;i++){
+                    if(nums[i]<nums[i-1]){
+                            dips++;  //checking dip at ith index    
+                            idx=i;
+                    }
+            }
+         if(dips==0) return true; // increasing array
+         else if(dips>=2) return false; // just one modification cannot make it non-decreasing
+         else{ //just one dip at idx th index
+                    if(idx==1 || idx==n-1) return true;
+                    if(nums[idx+1]>=nums[idx-1]) return true;
+                    else{
+                            if(nums[idx-2]>nums[idx]) return false;
+                            else return true;
+                        }       
+            }
+         return false;   
+            
+            
+    }
+};
