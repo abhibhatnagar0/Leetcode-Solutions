d78ac905af71b92b45f5d61adfe3908b3eceb676
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-05-29 17:25:20 +0530

    Time: 16 ms (50.06%), Space: 6 MB (33.85%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -1,16 +1,18 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-            if(left==0 || right==0) return 0;
-            int count=0;
+           
+            
             // when all bits at a idx of numbers btw left and right are 1, then only ans will               have 1 at that idx
             //when both left and right have 1 at a idx, all numbers btw them ll have the same
-            
+         int count=0;   
         while(left!=right){
                 left>>=1;
                 right>>=1;
                 count++;
         }
+             if(left==0 || right==0) return 0;//in worst case both, left,right will become 0
+                                              // means no common fixed bits
             return left<<count;
     }
 };
