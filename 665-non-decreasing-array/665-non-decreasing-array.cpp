892a97b507280a56d3cfede461b2d8b54bf49c53
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-05-29 20:20:28 +0530

    Time: 48 ms (21.64%), Space: 27.1 MB (57.20%) - LeetHub

--- 665-non-decreasing-array/665-non-decreasing-array.cpp
+++ 665-non-decreasing-array/665-non-decreasing-array.cpp
@@ -13,15 +13,12 @@ public:
          if(dips==0) return true; // increasing array
          else if(dips>=2) return false; // just one modification cannot make it non-decreasing
          else{ //just one dip at idx th index
-                    if(idx==1 || idx==n-1) return true;
-                    if(nums[idx+1]>=nums[idx-1]) return true;
+                    if(idx==1 || idx==n-1) return true; // we can no condition for decreasing or increasing 0 and n-1 indexs.
+                    if(nums[idx+1]>=nums[idx-1]) return true; //can put idx btw idx-1 and idx+1(inclusive)
                     else{
                             if(nums[idx-2]>nums[idx]) return false;
-                            else return true;
+                            else return true; //can put idx-1 btw idx-2 and idx(inclusive)
                         }       
-            }
-           
-            
-            
+            }      
     }
 };
