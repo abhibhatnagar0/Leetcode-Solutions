69d97273aa75c740d74863e88e69868f3f490341
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-05-30 13:15:03 +0530

    Time: 17 ms (43.42%), Space: 11.5 MB (19.72%) - LeetHub

---
+++ 47-permutations-ii/47-permutations-ii.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+        vector<vector<int>> allperm;
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+if(nums.size()==1) return {nums};  //for n numbers, n! permutations are possible
+            
+             helper(nums,0); //0 is position of index to be fixed
+            return allperm;     
+    }
+        void helper(vector<int>& nums, int pos){
+                //pos se phle wale indexes are all fixed, so dont look at them.
+                if(pos==nums.size()){ //when nth index is to be fixed or sb fix ho chuke hai
+                        allperm.push_back(nums); //return current status of nums after swappings
+                        return;
+                }
+                unordered_set<int> st;//for removing duplicates at a particular pos
+            for(int i=pos;i<nums.size();i++){ //iterate all numbers from pos to n-1 index and fix them
+                                              // one by one at pos index
+                    if(st.find(nums[i])!=st.end()) continue; //nums[i] ko already swap krva chuke h nums[pos] ke saath
+                    st.insert(nums[i]); //coming for the 1st time
+                    swap(nums[i],nums[pos]);  // nums[i] ko fix krna h at pos index
+                    helper(nums,pos+1);       
+                    swap(nums[i],nums[pos]);  //backtracking step after recursive call
+            }
+        }
+};
