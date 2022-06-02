0ca5de9775bf414ffff77530258e8b9da5c01505
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-02 12:54:38 +0530

    Time: 6 ms (79.66%), Space: 7.5 MB (42.28%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,9 +1,6 @@
 class Solution {
 public:
         vector<vector<string>> ans;
-        
-      
-        
         //each idx of ans has 1 solution of queens position on board
     vector<vector<string>> solveNQueens(int n) {
         // make a n*n board, each row is a string
@@ -12,9 +9,12 @@ public:
                for (int i = 0; i < n; i++){
                   board[i] = s;
                  }
-        vector<int>cols(n,0);
-        vector<int>leftTop(2*n-1,0);
+        vector<int>cols(n,0); //checks ki (row,col) ke upar wale cols me koi queen toh nhi hai
+        vector<int>leftTop(2*n-1,0); //checks ki (row,col) ke leftTop diagonal me koi queen toh nhi h
+            //both row,col saath me -- honge ..ie for(3,4)->(2,3) (1,2) (0,1) so (n-1)-(col-row) ranges from 0 to 2(n-1)
         vector<int>rightTop(2*n-1,0);
+            //when row--,col++ honge ..ie for(3,1)->(2,2) (1,3) (0,4) so col+row ranges from 0 to 2(n-1)
+            
         //take one row at a time and check
             dfs(0,board,n,cols,leftTop,rightTop);
             
@@ -44,7 +44,14 @@ public:
            }
         }
         
-        bool isValid(int row,int col,vector<string>&board, int n){
+        bool issValid(int row,int col,vector<string>&board, int n,vector<int>&cols,vector<int>&leftTop,vector<int>&rightTop){
+                if(cols[col]==1) return false;
+                if(leftTop[(n-1)-(col-row)]==1) return false;
+                if(rightTop[row+col]==1) return false;
+                return true;
+        }
+        
+       /* bool isValid(int row,int col,vector<string>&board, int n){
           //for a queen to be placed at (row,col)
           //no queen shld be there on that row, col and diagonal
           //same row not possible as one row me daalkr, we are 
@@ -74,11 +81,7 @@ public:
                 }
              return true;   
         }
+        */
         
-        bool issValid(int row,int col,vector<string>&board, int n,vector<int>&cols,vector<int>&leftTop,vector<int>&rightTop){
-                if(cols[col]==1) return false;
-                if(leftTop[(n-1)-(col-row)]==1) return false;
-                if(rightTop[row+col]==1) return false;
-                return true;
-        }
+
 };
