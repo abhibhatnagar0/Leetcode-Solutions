a46867d2ed6ba2ecd3036cdaa4fbaab1d721b839
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-03 11:29:42 +0530

    Time: 181 ms (6.39%), Space: 46.8 MB (5.04%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.cpp
+++ 49-group-anagrams/49-group-anagrams.cpp
@@ -3,14 +3,16 @@ public:
     vector<vector<string>> groupAnagrams(vector<string>& strs) {
         int n= strs.size();
            vector<vector<string>> ans;
-            // all anagrams have same sorted order
-            unordered_map<string,vector<string>> mp;
-            //{sorted order of str, all anagram strings}
+            
+            map <map<char,int>,vector<string>> mp;
+            
             
             for(auto s:strs){
-                    string temp=s;
-                    sort(temp.begin(),temp.end());
-                    mp[temp].push_back(s);
+                    map<char,int> fmp;
+                    for(char c:s){
+                            fmp[c]++;
+                    }
+                    mp[fmp].push_back(s);        
             }
             
             for(auto p:mp){
