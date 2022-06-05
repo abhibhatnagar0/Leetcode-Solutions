cc040a940ce293585158c8e9044d2d4947bdba8d
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-05 17:30:03 +0530

    Time: 7 ms (65.29%), Space: 7.7 MB (98.12%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1,0);
-            for(int i=0;i<=n;i++){
-                    ans[i]= __builtin_popcount(i);
+        vector<int> dp(n+1,0);
+            
+            for(int i=1;i<=n;i++){
+                    dp[i]= dp[i & (i-1)] + 1;
             }
-            return ans;
+            return dp;
+           //i & i-1 will have one set bit less than i 
     }
 };
