bd372fc3151f2ee2c92b9a8652c7bcf4edec118e
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-05 13:28:05 +0530

    Time: 31 ms (91.68%), Space: 25.7 MB (99.86%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -3,22 +3,7 @@ public:
     vector<int> sortedSquares(vector<int>& nums) {
          int n =nums.size();
          vector<int> ans (n,0) ;
-        // for a case where all nos are positive/negative
-        if(nums[n-1]<0){
         
-            for(int p=n-1;p>=0;p--){
-                ans[n-(p+1)]= nums[p]* nums[p];
-                
-            }
-        }
-          
-        if(nums[0]>=0){
-            for(int p=0;p<=n-1;p++){
-                ans[p]= nums[p]* nums[p];
-            }
-        }
-        if(ans[n-1]!=0) //agar aa gya ans ab tk then return
-            return ans;
         
             int l=0;
             int r=n-1;
