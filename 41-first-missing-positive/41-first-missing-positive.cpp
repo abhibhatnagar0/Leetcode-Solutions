4d498f50fd9abd88e1ff1e2eb3036bb88b83bb25
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-06 00:51:56 +0530

    Time: 226 ms (49.53%), Space: 82.9 MB (96.33%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,21 +1,25 @@
 class Solution {
 public:
+        //cyclic sort easy qn
     int firstMissingPositive(vector<int>& nums) {
         int n = nums.size();
-        
+            
+ //just put nos in [1,n] at their correct position
         for(int i = 0; i < n; ++i)
         {
-            if(nums[i] <= 0 || nums[i] > n) //ignore numbers not in range of {1, n-1}
+            if(nums[i] <= 0 || nums[i] > n) //ignore numbers not in range of {1, n}
                 continue;
             else
             {
-                if(nums[nums[i]-1]==nums[i])  //already in correct place, then skip (else infinite loop of swapping)
+                if(nums[nums[i]-1]==nums[i])
                     continue;
-                swap(nums[i], nums[nums[i] - 1]);
-                --i;                          //can't go forward without putting swapped number in correct place.
+                 swap(nums[i], nums[nums[i] - 1]);
+                    i--;
+                                       
             }
         }
-        
+  //starting se traversing pe, jo idx pe no nhi mila, that idx+1 is no.
+  //saare idx pe  correct no aaya, ie [1,n] all present in nums, so return n+1          
         for(int i = 0; i < n; ++i)
             if(i + 1 != nums[i])
                 return i + 1;
