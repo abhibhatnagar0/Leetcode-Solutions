f79a20ce008a18a8044670f7cacf8b54237bfedb
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-05 12:10:30 +0530

    Time: 4 ms (49.59%), Space: 7.6 MB (6.82%) - LeetHub

--- 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
+++ 24-swap-nodes-in-pairs/24-swap-nodes-in-pairs.cpp
@@ -11,17 +11,35 @@
 class Solution {
 public:
     ListNode* swapPairs(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-         
+         if(head==NULL || head->next==NULL) return head;
+            
+            ListNode* dummy=new ListNode(-1);
+            dummy->next=head;
+            
             ListNode*first=head, *second=head->next; 
-            ListNode* future= second->next;
+            ListNode* future= second->next, *prev=dummy;
             
-            second->next=first;
-            first->next=future;
-            
-             first->next=swapPairs(future);
-            
-           return second;
+            while(first && second){
+                    
+                    second->next=first;
+                    first->next=future;
+                    prev->next=second;
+                    
+                    prev=first;
+                    first=future;
+                    if(first==NULL) break;
+                    second=first->next;
+                    if(second==NULL) {  //for odd number of nodes
+                            prev->next=first; // as it is first ko daal do bs
+                            break;
+                    }
+                    future=second->next;
+                    
+                    
+                    
+            }
+            return dummy->next;
+           
             
     }
 };
