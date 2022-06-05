4ef218cb761ce517d21d1be11b9161cf5d4961a8
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-05 17:35:26 +0530

    Time: 11 ms (31.83%), Space: 7.7 MB (98.12%) - LeetHub

--- 338-counting-bits/338-counting-bits.cpp
+++ 338-counting-bits/338-counting-bits.cpp
@@ -4,9 +4,14 @@ public:
         vector<int> dp(n+1,0);
             
             for(int i=1;i<=n;i++){
-                    dp[i]= dp[i & (i-1)] + 1;
+                    if(i&1==1) //if i is odd
+                    dp[i]= dp[i>>1] + 1;  //dp[i]= dp[i/2]+1
+                    else
+                            dp[i]= dp[i>>1]; //dp[i]= dp[i/2]
             }
+            //for i=even, right shift of i/2 gives same set bits
+            //for i=7, set bits of 3 are equal to set bits of 6, adding 1 for 7 (even+1 me extra bit)
             return dp;
-           //i & i-1 will have one set bit less than i 
+           
     }
 };
