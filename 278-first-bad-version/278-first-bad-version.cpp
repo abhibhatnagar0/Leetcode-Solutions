7dd9bebd5d74233fb5ae7c344018c6125ffc708e
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-07 14:16:03 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (68.74%) - LeetHub

--- 278-first-bad-version/278-first-bad-version.cpp
+++ 278-first-bad-version/278-first-bad-version.cpp
@@ -5,15 +5,15 @@ class Solution {
 public:
     int firstBadVersion(int n) {
           int start=1,end=n;
+            int ans; //stores index of first occurence of bad version
         while (start<=end) {
             int mid=start+(end-start)/2;
             if (isBadVersion(mid)) {
-                    if(mid==1) return mid;
-                   else if (isBadVersion(mid-1)) end=mid-1;
-                   else return mid;
+                   ans=mid;
+                   end=mid-1;
             }
             else start=mid+1;
         }
-        return -1;
+        return ans;
     }
 };
