260541850f6cb35314eaa704fb69800c8a092575
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-07 13:06:05 +0530

    Time: 19 ms (77.55%), Space: 16.6 MB (81.03%) - LeetHub

--- 45-jump-game-ii/45-jump-game-ii.cpp
+++ 45-jump-game-ii/45-jump-game-ii.cpp
@@ -2,26 +2,20 @@ class Solution {
 public:
     int jump(vector<int>& nums) {
         int n=nums.size();
-            vector<int> dp(10001,-1);
-            dp[n-1]=0; // dp[i] stores no of min jumps req from ith index to reach n-1 index
-            for(int i=n-2;i>=0;i--){
-                    if(i+nums[i]>=n-1) dp[i]=1; // at ith index, we can go upto i+nums[i]
-                    else{              
-                            //going from i to j in 1 jump and then j to n-1
-                            int reach=i+nums[i];
-                            int j=i+1;
-                            int minJumps=INT_MAX; // stores minJumps from j to n-1
-                            while(j<=reach){  
-                                    if(dp[j]!=-1)
-                                minJumps=min(minJumps,dp[j]);
-                                    j++;
-                            }
-                            if(minJumps!=INT_MAX)
-                                dp[i]=1+minJumps;
+            if(n==1) return 0;
+            
+            int currReach= 0; 
+            int maxReach= 0;
+            int minJumps=0;
+            
+            for(int i=0;i<n-1;i++){
+                    maxReach=max(i+nums[i],maxReach);
+                    
+                    if(i==currReach){
+                            minJumps++;
+                            currReach=maxReach;
                     }
             }
-            if(dp[0] == INT_MAX) return 0;
-            else return dp[0];
-            
+      return minJumps;
     }
 };
