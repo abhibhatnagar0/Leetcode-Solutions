95038ba795d3fdae54cdfe5806aaf06980686a18
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-11 12:39:37 +0530

    Time: 271 ms (60.35%), Space: 98.6 MB (71.00%) - LeetHub

--- 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
+++ 1658-minimum-operations-to-reduce-x-to-zero/1658-minimum-operations-to-reduce-x-to-zero.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
     int minOperations(vector<int>& nums, int x) {
-    /*nums ki starting and ending se kuch ele nikalne hai
-      such that unka sum x ho, and minimum no of ele niklalne hai.
-      this is eq to 
-      finding a subarray in middle of nums of sum.... total-x
-      nums.size()-subarray.size()= no of operations (min) */
-            
-            int n= nums.size();
+         int n= nums.size();
             if(n==1) {
                     if(nums[0]==x) return 1;
                     else return -1;
@@ -16,22 +10,32 @@ public:
             for(auto p: nums) total+=p;
           
             if(total==x) return n;
+            if(total<x) return -1;
+            
             //finding subarray of maximun length with sum total-x
-            int sum=0; //prefix sum
-            unordered_map <int,int> mp; //{sum,idx}
-            mp[0]=-1; //subarray starting from 0 idx
+               //using sliding window
+            int target =total-x;
+            int j=0,i=0;
+            int sum=0;// stores sum of curr window
             int maxlen=0;
-            for(int i=0;i<n;i++){
-                    sum+=nums[i];
-                    if(mp.find(sum-(total-x))!=mp.end()){
-                            maxlen=max(maxlen,i-mp[sum-(total-x)]);
+            //j can expand window, i can shrink window 
+            while(j<n){
+                  sum+=nums[j];
+                    while(sum>target){
+                            sum -= nums[i++];
+                       //keep shrinking window until sum becomes less than or equal to target    
                     }
-                    mp[sum]=i;
+                    if(sum<target){
+                            j++;
+                    }
+                    else if(sum==target) {
+                        maxlen=max(maxlen,j-i+1);  
+                            j++;
+                    }
             }
-            if(maxlen==0) {
-                    //no subarray can have sum=total-x 
-                    return -1;
-            }
-            return n-maxlen; //gives min part of nums from left and right with sum X
+            if(maxlen==0) return -1;
+            
+            return n-maxlen;
+            
     }
 };
