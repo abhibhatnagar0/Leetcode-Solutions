f1d91a64cab76d4d0dfef135c000bcdf738c8a0b
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-11 19:16:09 +0530

    Time: 7 ms (44.13%), Space: 8.7 MB (89.28%) - LeetHub

--- 162-find-peak-element/162-find-peak-element.cpp
+++ 162-find-peak-element/162-find-peak-element.cpp
@@ -1,24 +1,18 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        int n=nums.size();
-            int s=0,e=n-1;
-            while(s<e){
-                    int m= s+ (e-s)/2;
-                    if(nums[m]<nums[m+1]){
-                            //ans right side me pkka aayega (excluding m)
-                            //left me bhi aa skta h, but right me for sure aayega
-                            s=m+1;
-                    }
-                    else if (nums[m+1]<nums[m]){
-                            e=m;
-                            //e=m for case where nums[m-1]<nums[m] ie m is peak
-                            //when nums[m-1]>nums[m].... ans will pkka be in left
-                    }
-             // nums[i] cannot be equal to nums[i+1]      acq
-                   
-            }
-            //when s==e==m
-            return s;
+         int n = nums.size();
+        if(n == 1) return 0;
+        if(n == 2)return nums[0] > nums[1] ? 0 : 1;
+        int i = 1, j = n-2;
+        while(i <= j){
+            int mid = j + (i-j)/2;
+                if(nums[mid] > nums[mid-1] && nums[mid] > nums[mid+1]) return mid;
+                else if(nums[mid+1] > nums[mid]) i = mid+1;
+                else if(nums[mid-1] >= nums[mid]) j = mid-1;
+        }
+        if(nums[0] > nums[1]) return 0;
+        else if(nums[n-1] > nums[n-2]) return n-1;
+        return -1;
     }
 };
