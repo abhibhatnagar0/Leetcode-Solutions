132ce236ad9039f5bf006e70c83d48eca4e9d77f
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-13 12:25:33 +0530

    Time: 11 ms (37.61%), Space: 9.1 MB (9.59%) - LeetHub

---
+++ 120-triangle/120-triangle.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+        
+    int minimumTotal(vector<vector<int>>& triangle) {
+        int n= triangle.size(); //no of levels
+            int maxj= triangle[n-1].size();
+            vector<vector<int>> dp(n, vector<int>(maxj,INT_MAX));
+            return helper(triangle,n,0,0,dp);
+    }
+        
+        int helper(vector<vector<int>>& triangle,int n, int i,int j,vector<vector<int>>& dp){
+              if(i==n || j==triangle[i].size()) return 0;
+                
+                 if(dp[i][j]!=INT_MAX) return dp[i][j];
+                
+                return dp[i][j]= min(helper(triangle,n,i+1,j,dp), helper(triangle,n,i+1,j+1,dp)) + triangle[i][j];
+              
+                }
+};
