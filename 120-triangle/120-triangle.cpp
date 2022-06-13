24656173a4e5d841577deec1874c80293e3431a2
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-13 12:54:19 +0530

    Time: 16 ms (11.39%), Space: 8.7 MB (55.37%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -2,15 +2,20 @@ class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
         int n= triangle.size(); //no of levels
-            //bottom up approach
-            //changed original vector
-            //space o(1)
-           // time o(n2)
+        int maxj= triangle[n-1].size();   
+           vector<int> dp(n);
+              for(int j=0;j<maxj;j++){
+                      dp[j]= triangle[n-1][j];
+              }
+            
             for(int i=n-2;i>=0;i--){
-                 for(int j=0;j<triangle[i].size();j++){
-                  triangle[i][j]= min(triangle[i+1][j],triangle[i+1][j+1]) + triangle[i][j];
-                            }
+                    for(int j=0;j<triangle[i].size();j++){
+                        dp[j]=min(dp[j],dp[j+1])+ triangle[i][j];
+                    }
             }
-            return triangle[0][0];
+            return dp[0];
     }
+        //bottom up approach
+        // space o(n), time o(n2)
+        //no modification done in original vector
 };
