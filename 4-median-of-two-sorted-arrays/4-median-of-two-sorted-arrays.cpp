3132ffd80a014e6068048217715dcba6be650a68
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-14 16:06:30 +0530

    Time: 40 ms (85.81%), Space: 89.2 MB (86.95%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.cpp
@@ -1,38 +1,51 @@
 class Solution {
 public:
     double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-          
-        vector<int> v= merged(nums1,nums2);
-            double med;
-            int size=v.size();
+            int n= nums1.size(); //smaller
+            int m= nums2.size(); //larger
+            if(n>m) return findMedianSortedArrays(nums2,nums1);
             
-            if(size%2==1){
-                    med= v[size/2]*1.00;
+            int l1,l2,r1,r2;
+            /*if n+m=10, left and right sides have 5-5 ele in total
+              if n+m =9, left will have 4, right will have 5 ele in total
+              */
+            int s=0, e=n; //as s se e cuts can be at n+1 spaces
+            while(s<=e){
+                 int cut1= s+ (e-s)/2; // cut1=3 means 3 ele of nums1 in left side
+                 int cut2= (n+m)/2 - cut1; 
+                    
+                 if(cut1!=0) l1=nums1[cut1-1];
+                 else l1= INT_MIN;
+                    
+                 if(cut1!=n) r1=nums1[cut1];
+                 else r1= INT_MAX;
+                    
+                 if(cut2!=0) l2=nums2[cut2-1];
+                 else l2= INT_MIN;
+                    
+                 if(cut2!=m) r2=nums2[cut2];
+                 else r2= INT_MAX;
+                    
+                    if(l1>r2){
+                            //left of nums1 ko chota kro...cut1 phle kro
+                            e=cut1-1;
+                    }
+                    else if(r1<l2){
+                            //right of nums1 ko bda kro..cut1 aage kro
+                            s=cut1+1;
+                    }
+                    else{
+                            //cuts are at correct positions
+                            if((n+m)%2 ==0){
+                                  return (max(l1,l2) + min(r1,r2))/2.0;  
+                            }                     
+                            else{
+                                    //in even len, right side has extra element
+                                    return min(r1,r2);
+                            }
+                    }
             }
-            else {
-                    int p= v[size/2]+v[(size/2)-1] ;
-                    med= p/2.00;
-            }
-            return med;
-            
+            cout<<l1;
+          return -1;  
     }
-        vector<int> merged(vector<int>& nums1, vector<int>& nums2){
-                int p1=0,p2=0;
-                int m=nums1.size();
-                int n=nums2.size();
-                if(m==0) return nums2;
-                if(n==0) return nums1;
-                vector<int> ans;
-                while(p1<m && p2<n){
-                        if(nums1[p1]<nums2[p2]) ans.push_back(nums1[p1++]);
-                        else  ans.push_back(nums2[p2++]);
-                }
-                if(p1==m){
-                        while(p2<n) ans.push_back(nums2[p2++]);
-                }
-                 if(p2==n){
-                        while(p1<m) ans.push_back(nums1[p1++]);
-                }
-                return ans;
-        }
 };
