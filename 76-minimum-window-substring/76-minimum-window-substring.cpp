76c10ea8a55efff62d85d6e59f9d219ea9d191af
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-16 22:48:15 +0530

    Time: 64 ms (22.05%), Space: 8.4 MB (29.37%) - LeetHub

--- 76-minimum-window-substring/76-minimum-window-substring.cpp
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -5,7 +5,7 @@ public:
         int m= t.size();
             
             string ans= "";
-            if(m>n) return ans;
+            if(m>n) return ans; //size of s shld be greater than that of t
             int minlen= INT_MAX;
             
         unordered_map<char,int> mp; //{char,freq} in t string
@@ -14,29 +14,35 @@ public:
             }
             
         unordered_map<char,int> mp2; //{char,freq} in curr substr of s
-            int count=0;
+            int count=0; //whenevr a char of t is found in s, count++
+            //count==t.size() means ki subarry mil gya 
             
         int i=0,j=0;
             while(j<n){
-                    mp2[s[j]]++;
+                    if(mp[s[j]]==0) { //for a char of s which is not in t
+                            j++;      //no need to store its freq in mp2
+                            continue;
+                    }
+                    else mp2[s[j]]++;
+                    
                     if(mp2[s[j]]<= mp[s[j]]){
                             count++;
                     }
                     
                     if(count==m){
-                            while(mp[s[i]]==0 || mp2[s[i]]>mp[s[i]]){
-                                    mp2[s[i]]--;
+                            while(mp[s[i]]==0 || mp2[s[i]]>mp[s[i]]){//shrinking subarray if either s[i] not in mp,
+                                    mp2[s[i]]--;                     // or mp2 have its freq more than req
                                     i++;
                             }
-                            if(j-i+1 < minlen){
-                                    minlen=j-i+1;
+                            if(j-i+1 < minlen){ //after shrinking (i,j) subarray is a candidate of being ans
+                                    minlen=j-i+1;  //if its len is smaller
                                     ans= s.substr(i,minlen);
                             }
                             
                      }
                    j++;      
             }
-            cout<<count;
+         
             return ans;
     }
 };
