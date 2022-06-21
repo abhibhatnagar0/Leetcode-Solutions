262ab0131a31242fa07d6b29dea3cd83340f1bdf
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-21 14:38:35 +0530

    Time: 210 ms (18.07%), Space: 54 MB (27.49%) - LeetHub

--- 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
+++ 1642-furthest-building-you-can-reach/1642-furthest-building-you-can-reach.cpp
@@ -3,7 +3,7 @@ public:
     int furthestBuilding(vector<int>& heights, int bricks, int ladders) {
         int n= heights.size();
             priority_queue<int> pq; //max heap
-            //will give max no of bricks used in 1 step in past
+            //will give max no of bricks used in any one step in past
             int i=0; //my curr position
             while(i<n-1){
                     if(heights[i+1]<=heights[i]){
@@ -13,21 +13,22 @@ public:
                     }
                     else{
                             int diff= heights[i+1]- heights[i];
-                            if(ladders==0 && bricks<diff){
+                            if(ladders==0 && bricks<diff){ //when I have no bricks and ladders left
                                     return i;
                             }
-                            else if(bricks>0 && diff<=bricks){
+                            else if(bricks>0 && diff<=bricks){ //ladders are of greater imp, so phle bricks use kro
                                     bricks-=diff;
-                                    pq.push(diff);
+                                    pq.push(diff);//if at any step, too many bricks used,that can be replaced with ladder
+                                                  //as ladders are better for large diff
                             }
-                            else {
+                            else { 
                                     if(ladders>0){
-                                            if(pq.size()>0 && pq.top()>diff){
-                                            bricks+=pq.top()-diff;
+                                            if(pq.size()>0 && pq.top()>diff){//if diff small and no bricks left
+                                            bricks+=pq.top()-diff; //use pq.top wali bricks here and ladder there
                                              pq.pop();
-                                             pq.push(diff);
+                                             pq.push(diff); //iss step ke liye 
                                             }
-                                            ladders--;
+                                            ladders--;//ye toh hona h hi, either iss step me ya large bricks wale step me
                                     }
                             }
                     }
