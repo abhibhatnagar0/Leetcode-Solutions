189359267ff32f75ff39d02519d99403c7a0761b
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-29 11:20:55 +0530

    Time: 28 ms (34.78%), Space: 14.2 MB (57.58%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.cpp
@@ -10,41 +10,26 @@
 class Solution {
 public:
     TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
-        if(root==NULL || root==p || root==q) return root;
-        vector<TreeNode*> path1;
-        vector<TreeNode*> path2;
-         TreeNode* ans;
-         int idx1 = 0, idx2 = 0;
-        if(getPath(root,p,path1) && getPath(root,q,path2)){
-             int size1 = path1.size();
-             int size2 = path2.size();
-        
-        while (idx1 < size1 && idx2 < size2) {
-            if (path1[idx1] == path2[idx2]) {
-                idx1++;
-                idx2++;
-                continue;
-            }
-            else break;
+         //base case
+        if (root == NULL || root == p || root == q) {
+            return root;
         }
-            idx1--;
+        //first try of finding lca only in left or righr subtree
+        TreeNode* left = lowestCommonAncestor(root->left, p, q);
+        TreeNode* right = lowestCommonAncestor(root->right, p, q);
+
+        //result
+        if(left == NULL) {
+            return right; //both p and q in right subtree
         }
-           return path1[idx1];   
-              
+        else if(right == NULL) {
+            return left;  //both p and q in left subtree
+        }
+        else { 
+     //both left and right are not null, both have one of p or q respectively
+            return root;
+        }
     }
-    bool getPath(TreeNode* root, TreeNode*p, vector<TreeNode*> &path){
-        if(root==NULL) return false;
-        
-           path.push_back(root);
-        if(root==p){
-            return true;
-        }
-        
-          if(getPath(root->left,p,path) || getPath(root->right,p,path)){
-              return true;
-          }
-           path.pop_back();
-        
-         return false;
-    }
+//that node jiske left se p/q, right se q/p return hoga..will be lca
+//if at a node,left side returns a node, and right is null, ie p,q both lies in left
 };
