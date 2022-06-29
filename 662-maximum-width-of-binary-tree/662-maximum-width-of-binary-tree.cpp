3858bcae9358ce2ec9f554b38fd1d60fedd4752f
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-06-29 18:04:57 +0530

    Time: 27 ms (6.51%), Space: 17.2 MB (85.28%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -11,6 +11,22 @@
  */
 class Solution {
 public:
+    /* width of BT is mox no of nodes that can come in btw 2 extreme nodes at a lvl.
+    we are giving idx to each node at each level...such that first node of each lvl
+    has 0 idx.
+    for this, we marked root as i=0 and its children as 2*i and 2*i+1.
+    This gives   0
+              0      1
+            0   1   2   3
+           0 1 2 3 4 5 6 7 
+        but this wont start from 0 if leftmost node is not there
+        which can cause overflow as after 31 levels, idxs of nodes will overflow.
+        a level can have start= 33.....end=69...width of this lvl= 69-33+1
+        we will make start=0 always and end idx= p.second-start
+        SO 
+          for next level idxs will be 2*idx and 2*idx+1.
+    
+    */
     int widthOfBinaryTree(TreeNode* root) {
         if(root==NULL) return 0;
         int maxWidth=INT_MIN;
@@ -18,15 +34,16 @@ public:
         q.push({root,0});
         
         while(!q.empty()){
-            int size= q.size();
-            int s = q.front().second;
-            int e = q.back().second;
+            int size= q.size(); //curr level ka size
+            int s = q.front().second; //curr level ka smallest existing index
+            int e = q.back().second;  //curr level la largest existing index
             maxWidth = max(maxWidth, e - s + 1);
            
-            for(int i=0;i<size;i++){
+         for(int i=0;i<size;i++){//adding children of all nodes of curr lvl in queue
+             
                  pair<TreeNode*, int> p= q.front();
                  q.pop();
-                int idx= p.second - s;
+                int idx= p.second - s; //to make curr lvl start from 0
                 if(p.first->left !=NULL){
                     q.push({p.first->left, (long long)2*idx});
                 }
