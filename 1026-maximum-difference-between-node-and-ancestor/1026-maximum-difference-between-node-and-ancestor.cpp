c51aa3ca18706883320c2e3264e826b71afb1aa2
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-02 19:03:31 +0530

    Time: 11 ms (34.79%), Space: 11.1 MB (27.51%) - LeetHub

---
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxdiff=INT_MIN;
+    
+    int maxAncestorDiff(TreeNode* root) {
+        helper(root);
+        return maxdiff; //max-min
+    }
+    pair<int,int> helper(TreeNode* root){
+        if(root==NULL) return {INT_MIN,INT_MAX};
+        if(root->left==NULL && root->right==NULL){
+            return {root->val, root->val};
+        }
+        pair<int,int> left= helper(root->left);
+        pair<int,int> right= helper(root->right);
+        
+        int minOfchildren= min(left.second,right.second);
+        int maxOfchildren= max(left.first,right.first);
+        int diff= max(abs(root->val-minOfchildren), abs(root->val-maxOfchildren));
+        maxdiff= max(diff,maxdiff);
+        
+        return {max(root->val,maxOfchildren),min(root->val,minOfchildren)};
+        
+    }
+};
