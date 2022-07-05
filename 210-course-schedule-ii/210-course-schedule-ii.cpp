b6234e9d21531da43e8ef21a40641e0eaa5925f6
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-05 21:34:40 +0530

    Time: 36 ms (42.72%), Space: 13.4 MB (81.76%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.cpp
+++ 210-course-schedule-ii/210-course-schedule-ii.cpp
@@ -1,47 +1,46 @@
 class Solution {
 public:
     vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
-         int m= numCourses;
-         
-        vector<vector<int>> adj(m);
-        vector<int> indegree(m,0);
+        int n= numCourses;
+        vector<vector<int>> adj(n);
+    vector<int> indegree(n, 0);
+    
+    for(int i=0; i<prerequisites.size(); i++){
+        int u = prerequisites[i][0];
+        int v = prerequisites[i][1];
         
-        for(auto v:prerequisites){
-            adj[v[1]].push_back(v[0]);
-            indegree[v[0]]++;
-        }
-        vector<bool> visited(m,false);
-        queue<int> q;
-       
-        for(int i=0;i<m;i++){
-            if(indegree[i]==0){
-                q.push(i);
-                visited[i]=true;
-            }
-        }
-        vector<int> ans;
-        while(!q.empty()){
-            int node = q.front();
-            q.pop();
-            ans.push_back(node);
-            for(auto neighbour :adj[node]){
-                if(visited[neighbour]==0){
-                    indegree[neighbour]--;
-                    if(indegree[neighbour]==0){
-                        q.push(neighbour);
-                        visited[neighbour]=true;
-                    }
-                }
-            }
-        }
-        
-    if(ans.size() == m){
+        adj[v].push_back(u);
+        indegree[u]++;
+    }
+    
+    vector<int> ans;
+    bfs(adj, indegree, ans);
+    
+    if(ans.size() == n){
         return ans;
     }
     else{
         ans.clear();
         return ans;
+      }
     }
+    
+    void bfs(vector<vector<int>> &adj, vector<int> &indegree, vector<int> &ans){
+    queue<int> q;
+    for(int i=0; i<indegree.size(); i++){
+        if(indegree[i] == 0) q.push(i);
+    }
+    
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
         
+        ans.push_back(front);
+        
+        for(auto i : adj[front]){
+            indegree[i]--;
+            if(indegree[i] == 0) q.push(i);
+        }
     }
+}
 };
