524feac4f3920b791da71e9940850f6e9100a243
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-05 19:39:27 +0530

    Time: 248 ms (50.83%), Space: 47.3 MB (52.56%) - LeetHub

--- 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
+++ 802-find-eventual-safe-states/802-find-eventual-safe-states.cpp
@@ -1,24 +1,35 @@
 class Solution {
 public:
-     bool dfs(vector<vector<int>>& graph,vector<int>& color,int node)
+     bool dfs(vector<vector<int>>& graph,vector<int>& color,int node, vector<bool> &hasCycle)
     {
         color[node]=1;  // Initially assume that the node is unsafe.
         
         for(auto cur : graph[node])
-            if((color[cur]==0 && dfs(graph,color,cur)) || color[cur]==1)
+            if((color[cur]==0 && dfs(graph,color,cur,hasCycle)) || color[cur]==1){
+                hasCycle[node] =true;
                 return true;
+            }
         
         color[node]=2;  // If there is no cycle mark node as safe.
+        hasCycle[node]=false;
         return false;
     }
     
     vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
-        vector<int> color(graph.size()),result;
-		
+        int n=graph.size();
+        vector<int> color(n,0) ,result;
+		vector<bool> hasCycle(n,false);
 		// If node is visited and safe or unvisited and does not contain cycle then add it to the answer.
-        for(int i=0;i<graph.size();i++)
-            if(color[i]==2 ||  !dfs(graph,color,i))
+        
+         for(int i=0;i<graph.size();i++){
+             if(color[i]==0) dfs(graph,color,i,hasCycle);
+         }
+            
+        
+        for(int i=0;i<graph.size();i++){
+            if(color[i]==2 || hasCycle[i]==false)
                 result.push_back(i);
+        }
                 
         return result;
     }
