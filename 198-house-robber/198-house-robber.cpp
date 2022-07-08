b86e4bf37e207c69b08d8c9ff4a4e14a48197b93
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-08 15:47:12 +0530

    Time: 5 ms (22.72%), Space: 7.7 MB (75.71%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,16 +2,11 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n= nums.size();
-        
-        //dp[i] means max value gained if i houses are there to rob
-        int prevbytwo =0; //total 0 house dp[0]
-        int prevbyone =nums[0]; //total 1 house dp[1]
-        for(int i=1;i<n;i++){
-            int curr = max(prevbytwo +nums[i], prevbyone);//dp[2]
-            
-            prevbytwo= prevbyone;
-            prevbyone= curr;
-        }
-        return prevbyone;//=curr
+        if(n==0) return 0;
+        if(n==1) return nums[0];
+        if (nums.size() >= 2) nums[1] = max(nums[0], nums[1]);
+ for (int i = 2; i < nums.size(); ++i)
+ nums[i] = max(nums[i - 1], nums[i - 2] + nums[i]);
+ return nums.back() ;
     }
 };
