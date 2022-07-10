cf6b7cde840980029c45c5454afab63341d274a4
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-10 09:38:12 +0530

    Time: 16 ms (22.15%), Space: 13.8 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -5,11 +5,20 @@ public:
         vector<int>dp(n+1,0);
         if(n==0 || n==1) return 0;
         //dp[i] stores i tk pahuchne ke liye cost required
-        dp[0]=0;
-        dp[1]=0; //we can start from 0 or 1
+        // dp[0]=0;
+        // dp[1]=0; //we can start from 0 or 1
+        // for(int i=2;i<=n;i++){
+        //     dp[i] = min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+        // }
+        // return dp[n];
+        int prevbytwo=0;
+        int prevbyone=0;
         for(int i=2;i<=n;i++){
-            dp[i] = min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+            int curr = min(cost[i-1]+prevbyone,cost[i-2]+prevbytwo);
+            prevbytwo=prevbyone;
+            prevbyone=curr;
         }
-        return dp[n];
+        return prevbyone;
+        
     }
 };
