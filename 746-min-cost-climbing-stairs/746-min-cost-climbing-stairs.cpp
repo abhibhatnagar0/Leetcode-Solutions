bcf26e671fb8e287cd79de0971c296ca4e18fd51
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-10 09:34:16 +0530

    Time: 8 ms (68.42%), Space: 13.8 MB (28.79%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
-    
     int minCostClimbingStairs(vector<int>& cost) {
-        int n= cost.size();// destination
-        vector<int> dp(n+1,-1);
-            int op1= helper(0,n,cost,dp);
-            int op2= helper(1,n,cost,dp);
-        return min(op1,op2);
-    }
-    int helper(int i,int n, vector<int>& cost, vector<int> &dp){
-        if(i==n) return 0;
-        if(i>n) return 10000; //INF
-        if(dp[i]!=-1) return dp[i];
-        return dp[i]= min(helper(i+1,n,cost,dp)+ cost[i], helper(i+2,n,cost,dp) + cost[i]);
+        int n= cost.size(); //destination
+        vector<int>dp(n+1,0);
+        if(n==0 || n==1) return 0;
+        //dp[i] stores i tk pahuchne ke liye cost required
+        dp[0]=0;
+        dp[1]=0; //we can start from 0 or 1
+        for(int i=2;i<=n;i++){
+            dp[i] = min(cost[i-1]+dp[i-1],cost[i-2]+dp[i-2]);
+        }
+        return dp[n];
     }
 };
