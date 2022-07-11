abe619238de0068e50edad6218a608ef7997c486
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-11 09:05:36 +0530

    Time: 0 ms (100.00%), Space: 7.6 MB (75.74%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -2,11 +2,14 @@ class Solution {
 public:
     int rob(vector<int>& nums) {
         int n= nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        if (nums.size() >= 2) nums[1] = max(nums[0], nums[1]);
- for (int i = 2; i < nums.size(); ++i)
- nums[i] = max(nums[i - 1], nums[i - 2] + nums[i]);
- return nums.back() ;
+        // vector<int> dp(n+1,0);
+        //dp[i] means max value gained if i houses are there to rob
+        int dp[2]={0};
+        dp[0]=0;
+        dp[1]=nums[0];
+        for(int i=2;i<=n;i++){
+            dp[i%2]= max(dp[(i-2)%2]+nums[i-1], dp[(i-1)%2]);
+        }
+        return dp[n%2];
     }
 };
