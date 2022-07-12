2431a36bcfc6ba2e506daa1b5ea475bff8058818
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-12 13:06:12 +0530

    Time: 135 ms (23.38%), Space: 16.4 MB (96.94%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -9,8 +9,7 @@ public:
         int maxlen = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0) dp[i][j]= matrix[i][j]=='1'? 1 : 0;
-                else if(j==0) dp[i%2][j]= matrix[i][j]=='1'? 1 : 0;
+                if(i==0 || j==0) dp[i%2][j]= matrix[i][j]=='1'? 1 : 0;
                 else{
                     if(matrix[i][j]=='1')
                     dp[i%2][j]= 1 + min(dp[(i-1)%2][j],min(dp[i%2][j-1],dp[(i-1)%2][j-1]));
