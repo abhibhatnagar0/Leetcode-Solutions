7784aa126c6c14c925a7172f0b3f86798c79520d
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-12 13:05:35 +0530

    Time: 86 ms (67.18%), Space: 16.5 MB (93.24%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -3,18 +3,22 @@ public:
     int maximalSquare(vector<vector<char>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
+        vector<vector<int>>dp(2,vector<int>(n,0));
         //dp[i][j] gives length of max sq that can be made using matrix[i][j] as 
         //bottom right element
         int maxlen = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(i==0 || j==0) dp[i][j]= matrix[i][j]=='1'? 1 : 0;
+                if(i==0) dp[i][j]= matrix[i][j]=='1'? 1 : 0;
+                else if(j==0) dp[i%2][j]= matrix[i][j]=='1'? 1 : 0;
                 else{
                     if(matrix[i][j]=='1')
-                    dp[i][j]= 1 + min(dp[i-1][j],min(dp[i][j-1],dp[i-1][j-1]));
+                    dp[i%2][j]= 1 + min(dp[(i-1)%2][j],min(dp[i%2][j-1],dp[(i-1)%2][j-1]));
+                    else{
+                        dp[i%2][j]=0;
+                    }
                 }
-                maxlen= max(maxlen,dp[i][j]);
+                maxlen= max(maxlen,dp[i%2][j]);
             }
         }
         return maxlen*maxlen;
