8ab14c445605c58661fb0cce10f349c603432c68
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-12 23:21:46 +0530

    Time: 200 ms (55.05%), Space: 54.9 MB (90.80%) - LeetHub

--- 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
+++ 714-best-time-to-buy-and-sell-stock-with-transaction-fee/714-best-time-to-buy-and-sell-stock-with-transaction-fee.cpp
@@ -3,8 +3,8 @@ public:
     int maxProfit(vector<int>& prices, int fee) {
         int n= prices.size();
         if(n<=1) return 0;
-        vector<int> buy(n,0);
-        vector<int> sell(n,0);
+        vector<int> buy(2,0);
+        vector<int> sell(2,0);
         // buy[i] represents the max profit at day i in buy status
         // (the last action you took is a buy action)
         // sell[i] represents the max profit at day i in sell status
@@ -14,11 +14,11 @@ public:
         buy[0]= -prices[0];
         sell[0]= 0;
         for(int i=1;i<n;i++){
-            buy[i]=max(buy[i-1],sell[i-1]- prices[i]);
+            buy[i%2]=max(buy[(i-1)%2],sell[(i-1)%2]- prices[i]);
             // keep the same as day i-1, or buy from sell status at day i-1
-            sell[i]=max(sell[i-1],buy[i-1]+prices[i]-fee);
+            sell[i%2]=max(sell[(i-1)%2],buy[(i-1)%2]+prices[i]-fee);
             // keep the same as day i-1, or sell from buy status at day i-1
         }
-        return sell[n-1];
+        return sell[(n-1)%2];
     }
 };
