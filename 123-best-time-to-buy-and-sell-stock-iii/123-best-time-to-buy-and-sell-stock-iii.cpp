8c6fa38406e705ec9eca993750f35c42eb024381
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-12 20:26:15 +0530

    Time: 171 ms (91.37%), Space: 81.4 MB (57.79%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-         int firstBuy=INT_MAX,profit1=0,secondBuy=INT_MAX,profit2=0;
-		
-        for(int currPrice : prices)
-        {
-            firstBuy=min(firstBuy,currPrice);   // minimum price for 1st buy (min so far)
-            profit1=max(profit1,currPrice-firstBuy);    
-            // maximum profit after 1st sell= max(earlier profit1, profit1 after selling on currDay)
-            secondBuy=min(secondBuy,currPrice-profit1);        
-            // minimum price for 2nd buy after removing profit from 1st sell(effective buying cost)
-            profit2=max(profit2,currPrice-secondBuy);      // maximum profit after 2nd sell
-           
+        int n= prices.size();
+        //Atmost 2 transaction
+        //both left and right take care of 1-1 transaction
+        vector<int>left(n,0);
+        //left[i] means max profit earned so far till i by selling on i or before
+        //buying at min so far from starting
+        left[0]=0;
+        int minBefore=prices[0];//buying price
+        for(int i=1;i<n;i++){
+            minBefore= min(minBefore,prices[i]);
+            left[i]= max(left[i-1], prices[i]-minBefore);
         }
-         return profit2;
+        
+        vector<int> right(n,0);
+        //right[i] means max profit earned by buying on i or before
+        //selling at max so far from end
+        right[n-1]=0;
+        int maxAfter= prices[n-1]; //selling price
+        for(int i=n-2;i>=0;i--){
+            maxAfter= max(maxAfter,prices[i]);
+            right[i]= max(right[i+1], maxAfter-prices[i]);
+        }
+        int maxProfit=0;
+        for(int i=0;i<n;i++){
+            int profit= left[i] + right[i];
+            maxProfit=max(profit,maxProfit);
+        }
+        return maxProfit;
     }
+    //we can buy and sell on the same day as well
 };
