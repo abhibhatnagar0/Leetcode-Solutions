fb0af631dfc6775fe6368cffdd34f8f88bae9c8a
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-13 19:19:58 +0530

    Time: 31 ms (13.33%), Space: 6 MB (5.62%) - LeetHub

--- 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
+++ 201-bitwise-and-of-numbers-range/201-bitwise-and-of-numbers-range.cpp
@@ -1,18 +1,28 @@
 class Solution {
 public:
     int rangeBitwiseAnd(int left, int right) {
-           
-            
-            // when all bits at a idx of numbers btw left and right are 1, then only ans will               have 1 at that idx
+            if(left==0 || right==0) return 0;
+        //if left right have different bits ...ans=0
+        //ie all flipped bits
+        if((int)log2(left)!= (int)log2(right)) return 0;
+        // 00111001
+        // 01000000 (this no will be in btw of my range)..hence ans=0
+        // 01000011 
+        
+        
+        int count=0;
+            // when all bits at a idx of numbers btw left and right are 1, then only ans will have 1 at that idx
             //when both left and right have 1 at a idx, all numbers btw them ll have the same
-         int count=0;   
+
+        //when nos have same bits 
         while(left!=right){
                 left>>=1;
                 right>>=1;
                 count++;
         }
-             if(left==0 || right==0) return 0;//in worst case both, left,right will become 0
-                                              // means no common fixed bits
-            return left<<count;
+        //count gives no of flipped bits (from right)
+        //left now will have fixed bits possessed by all nos in range
+            return left<<count; 
     }
+    //while loop will run max of 32 times
 };
