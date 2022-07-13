50fc6b398210deefda5ffc31d87af33af3d3382a
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-13 09:56:58 +0530

    Time: 257 ms (65.71%), Space: 75.3 MB (76.77%) - LeetHub

--- 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
+++ 123-best-time-to-buy-and-sell-stock-iii/123-best-time-to-buy-and-sell-stock-iii.cpp
@@ -2,33 +2,24 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n= prices.size();
-        //Atmost 2 transaction
-        //both left and right take care of 1-1 transaction
-        vector<int>left(n,0);
-        //left[i] means max profit earned so far till i by selling on i or before
-        //buying at min so far from starting
-        left[0]=0;
-        int minBefore=prices[0];//buying price
-        for(int i=1;i<n;i++){
-            minBefore= min(minBefore,prices[i]);
-            left[i]= max(left[i-1], prices[i]-minBefore);
-        }
-        
-        vector<int> right(n,0);
-        //right[i] means max profit earned by buying on i or before
-        //selling at max so far from end
-        right[n-1]=0;
-        int maxAfter= prices[n-1]; //selling price
-        for(int i=n-2;i>=0;i--){
-            maxAfter= max(maxAfter,prices[i]);
-            right[i]= max(right[i+1], maxAfter-prices[i]);
-        }
-        int maxProfit=0;
-        for(int i=0;i<n;i++){
-            int profit= left[i] + right[i];
-            maxProfit=max(profit,maxProfit);
-        }
-        return maxProfit;
+        if(n<=1) return 0;
+     	// All the variables below are in terms of profit we have 
+	// Bought for the first time
+	int bought1 = -1 * prices[0];
+	// Sold for the first time
+	int sold1 = 0;
+	// Bought for the second time
+	int bought2 = INT_MIN;
+	// Sold for the second time
+	int sold2 = 0;
+
+	for(int i = 1; i < prices.size(); i++) {
+		bought1 = max(bought1, -1 * prices[i]);
+		sold1 = max(sold1, bought1 + prices[i]);
+		bought2 = max(bought2, sold1 - prices[i]);
+		sold2 = max(sold2, bought2 + prices[i]);
+	}
+
+	return sold2;
     }
-    //we can buy and sell on the same day as well
 };
