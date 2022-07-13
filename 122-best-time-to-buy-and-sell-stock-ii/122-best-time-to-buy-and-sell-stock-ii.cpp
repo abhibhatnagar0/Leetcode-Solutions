33826723da6cb9ef8783e02a69a76239556239ed
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-13 09:17:52 +0530

    Time: 28 ms (5.81%), Space: 13.1 MB (23.63%) - LeetHub

---
+++ 122-best-time-to-buy-and-sell-stock-ii/122-best-time-to-buy-and-sell-stock-ii.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+         int n= prices.size();
+        if(n<=1) return 0;
+        vector<int> buy(2,0);
+        vector<int> sell(2,0);
+        // buy[i] represents the max profit at day i in buy status
+        // (the last action you took is a buy action)
+        // sell[i] represents the max profit at day i in sell status
+        // (the last action you took is a sell action)
+        
+        buy[0]= -prices[0];
+        sell[0]= 0;
+        for(int i=1;i<n;i++){
+            buy[i%2]=max(buy[(i-1)%2],sell[(i-1)%2]- prices[i]);
+            // keep the same as day i-1, or buy from sell status at day i-1
+            sell[i%2]=max(sell[(i-1)%2],buy[(i-1)%2]+prices[i]);
+            // keep the same as day i-1, or sell from buy status at day i-1
+        }
+        return sell[(n-1)%2];
+    }
+};
