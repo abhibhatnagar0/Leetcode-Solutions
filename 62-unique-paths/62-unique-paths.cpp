5e2f9fd149093afbf24a92e38a66a67ddf07f633
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-13 13:18:27 +0530

    Time: 0 ms (100.00%), Space: 6.1 MB (57.40%) - LeetHub

--- 62-unique-paths/62-unique-paths.cpp
+++ 62-unique-paths/62-unique-paths.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        //m-1 times down jana hai
-        //n-1 times right jana hai 
-       /* _ _ _ _ _ _ _ _ total m+n-2 spaces
-       to be filled with R and D
-       total ways to fill R = (m+n-2)C m-1 or (m+n-2)C n-1 */
-            int N = m+n-2;
-            int r = m-1;
-            double ans=1;
-            for(int p=1;p<=r;p++){
-                   ans=ans * (N-r+p)/p; 
+        if(m==1 || n==1) return 1;
+        vector<vector<int>>dp(2,vector<int>(n,1));
+        // dp[i][j] gives no of unique paths from (0,0) to (i,j)
+        // dp[0][any no]=1
+        // dp[any no][0]=1
+        for(int i=1;i<m;i++){
+            for(int j=1;j<n;j++){
+                dp[i%2][j]= dp[(i-1)%2][j]+ dp[i%2][j-1];
             }
-            return ans;
+        }
+        return dp[(m-1)%2][n-1];
     }
 };
