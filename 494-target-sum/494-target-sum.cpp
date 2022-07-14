c2cf65128388eadbb29256d00c7a1396e669a5dd
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-14 12:16:15 +0530

    Time: 27 ms (54.05%), Space: 12.5 MB (33.96%) - LeetHub

---
+++ 494-target-sum/494-target-sum.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    int findTargetSumWays(vector<int>& nums, int target) {
+        //make 2 subsets s1(all +ve signs), s2(all -ve signs)
+            target = abs(target);//if target is neg, reverse sign of subsets
+        //count will be same
+        
+        int no_of_zeros=0; //zeros have to be handled separately
+        long long sum=0;
+        for(int i=0; i<nums.size(); i++){
+            sum+=nums[i];
+            if(nums[i]==0) no_of_zeros++;
+        }
+        // s1 + s2 = sum
+        //s1-s2= target
+        //2(s1)= sum+target ie sum+target has to be even
+        
+        long long sum_s1= (sum+target)/2;
+        if(target > sum || (sum + target) % 2 == 1)   return 0;
+        return count_subsets(nums,sum_s1, no_of_zeros);
+        
+    }
+     //count no of subsets of nums with given sum   
+    int count_subsets(vector<int>& nums, long long sum, int x){
+        int n= nums.size();
+        vector<vector<int>> dp(n+1,vector<int>(sum +1,0));
+    //dp[i][j] gives no of subsets with i elements of nums giving sum j
+        //at dp[i][j] we are taking decision for nums[i-1]
+        //if it wants to contribute its sum or not
+        for(int i=0; i<=n;i++)
+            dp[i][0]=1;
+        for(int j=1; j<=sum; j++)
+            dp[0][j]=0;  
+            
+        for(int i=1;i<=n;i++){
+            for(int j=1; j<=sum; j++){
+                if(nums[i-1]>j || nums[i-1]==0) //0s will be considered later
+                        dp[i][j]= dp[i-1][j]; //not including nums[i-1]
+                else
+                        dp[i][j]= dp[i-1][j] + dp[i-1][j-nums[i-1]] ;
+           //sum of count of subsets by inc and exc nums[i-1]
+            }
+        }
+      return dp[n][sum]*(pow(2,x));
+        //each 0 has 2 options to go in subset or not
+    }
+};
