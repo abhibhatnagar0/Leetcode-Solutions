af472e9509e43e017ec8a734acd1afb4745d6e41
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-14 16:38:58 +0530

    Time: 172 ms (30.89%), Space: 50.2 MB (7.47%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
+    int solve(vector<int>& coins, int amount, int n, vector<vector<int>>&dp){
+        // if amount is 0, then we need to take 0 coins to fulfill the amount
+        if(amount == 0) return 0;
+		// if no coins present, then we need to take infinite coins to fulfill the amount
+        if(n == 0) return INT_MAX-1;
+		// if already present in dp vector, we will directly return the value
+        if(dp[n][amount] != -1) return dp[n][amount];
+		// if the present coin is smaller than the amount, we can either take it or decide not to take it
+        if(coins[n-1] <= amount){
+            return dp[n][amount] = min(1 + solve(coins, amount-coins[n-1], n, dp), solve(coins, amount, n-1, dp));
+        }
+		// else if the coin is larger than the amount, we can't take it
+        else{
+            return dp[n][amount] = solve(coins, amount, n-1, dp);
+        }
+    }
+    // unbounded knapsack
     int coinChange(vector<int>& coins, int amount) {
-         int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1));
-        for(int i=0; i<n+1; i++){
-            for(int j=0; j<amount+1; j++){
-                // if no coins present, then we need to take infinite coins to fulfill the amount
-                // if amount is 0, then we need to take 0 coins to fulfill the amount
-                if(j==0) dp[i][j] = 0;
-                if(i==0) dp[i][j] = INT_MAX-1;
-            }
-        }
-        
-        for(int i=1; i<n+1; i++){
-            for(int j=1; j<amount+1; j++){
-                // if the present coin is smaller than the amount, we can either take it or decide not to take it
-                // else if the coin is larger than the amount, we can't take it
-                if(coins[i-1] <= j){
-                    // choose minimum number of coisn required
-                    dp[i][j] = min(dp[i-1][j], 1 + dp[i][j-coins[i-1]]);
-                }
-                else{
-                    dp[i][j] = dp[i-1][j];
-                }
-            }
-        }
-        // if the answer is infinte only, that means, we could not find a way to take coins to fulfill the amount
-        return dp[n][amount] == (INT_MAX-1)? -1 : dp[n][amount];
+        int n = coins.size();
+        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
+        return solve(coins,amount,n,dp) == (INT_MAX-1) ? -1 : solve(coins,amount,n,dp);
     }
 };
