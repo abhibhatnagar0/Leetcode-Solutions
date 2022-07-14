8eed70a01e289e16933417e53cc058fb678a4c55
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-14 13:43:13 +0530

    Time: 173 ms (75.91%), Space: 11.3 MB (77.52%) - LeetHub

---
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canPartition(vector<int>& nums) {
+        int n= nums.size();
+        int sum = 0;
+        for (auto a : nums) // Sum up the array
+            sum += a;
+        
+        if (sum % 2) // If the sum is odd - we can never find two equal partitions
+            return false;
+        
+        sum /= 2;
+        return subsetsum(nums,sum,n);
+    }
+    
+    //finding if a subset is possible with given sum
+     bool subsetsum(vector<int> &nums, int sum, int n)
+    {
+        bool dp[n + 1][sum + 1];
+        for (int i = 0; i < n + 1; ++i) dp[i][0] = 1;
+        for (int j = 1; j < sum + 1; ++j) dp[0][j] = 0;
+
+        for (int i = 1; i < n + 1; ++i){
+            for (int j = 1; j < sum + 1; ++j){
+                if (nums[i - 1] <= j)
+                    dp[i][j] = dp[i - 1][j] || dp[i - 1][j - nums[i - 1]];
+                else
+                    dp[i][j] = dp[i - 1][j];
+            }
+        }
+        return dp[n][sum];
+     }
+};
