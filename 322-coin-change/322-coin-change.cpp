fc524e6105e56f9723ee2ee3e0d6497703a6f0f6
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-14 18:03:23 +0530

    Time: 185 ms (26.12%), Space: 14.5 MB (42.39%) - LeetHub

--- 322-coin-change/322-coin-change.cpp
+++ 322-coin-change/322-coin-change.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& coins, int amount, int n, vector<vector<int>>&dp){
-        // if amount is 0, then we need to take 0 coins to fulfill the amount
-        if(amount == 0) return 0;
-		// if no coins present, then we need to take infinite coins to fulfill the amount
-        if(n == 0) return INT_MAX-1;
-		// if already present in dp vector, we will directly return the value
-        if(dp[n][amount] != -1) return dp[n][amount];
-		// if the present coin is smaller than the amount, we can either take it or decide not to take it
-        if(coins[n-1] <= amount){
-            return dp[n][amount] = min(1 + solve(coins, amount-coins[n-1], n, dp), solve(coins, amount, n-1, dp));
+    int coinChange(vector<int>& coins, int n) {
+        vector<int> dp(n+1,10010);
+        dp[0]=0;
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<coins.size();j++)
+            {
+                if(i-coins[j]>=0)
+                {
+                    dp[i] = min(dp[i], dp[i-coins[j]]+1);
+                }
+            }
         }
-		// else if the coin is larger than the amount, we can't take it
-        else{
-            return dp[n][amount] = solve(coins, amount, n-1, dp);
-        }
-    }
-    // unbounded knapsack
-    int coinChange(vector<int>& coins, int amount) {
-        int n = coins.size();
-        vector<vector<int>>dp(n+1,vector<int>(amount+1,-1));
-        return solve(coins,amount,n,dp) == (INT_MAX-1) ? -1 : solve(coins,amount,n,dp);
+        return dp[n]>10000?-1:dp[n];
     }
 };
