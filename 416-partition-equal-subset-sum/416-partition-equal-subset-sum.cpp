d284d1c04d0b95560e7a72f2b027bb00c6f6bca1
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-14 13:47:02 +0530

    Time: 120 ms (87.79%), Space: 9.2 MB (89.18%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -1,8 +1,7 @@
 class Solution {
 public:
     bool canPartition(vector<int>& nums) {
-        int n= nums.size();
-        int sum = 0;
+         int sum = 0;
         for (auto a : nums) // Sum up the array
             sum += a;
         
@@ -10,24 +9,15 @@ public:
             return false;
         
         sum /= 2;
-        return subsetsum(nums,sum,n);
+        vector<bool> dp(sum+1, false); // dp keeps for each number if it has a subset or not
+        dp[0] = true;
+        
+        for (auto a : nums) {
+            for (int i = sum; i >= a; i--) {
+                dp[i] = dp[i] || dp[i-a]; // for each number, either we use it or we don't
+            } 
+        }
+        return dp[sum];
     }
     
-    //finding if a subset is possible with given sum
-     bool subsetsum(vector<int> &nums, int sum, int n)
-    {
-        bool dp[2][sum + 1];
-        for (int i = 0; i < 2; ++i) dp[i][0] = 1;
-        for (int j = 1; j < sum + 1; ++j) dp[0][j] = 0;
-
-        for (int i = 1; i < n + 1; ++i){
-            for (int j = 1; j < sum + 1; ++j){
-                if (nums[i - 1] <= j)
-                    dp[i%2][j] = dp[(i - 1)%2][j] || dp[(i - 1)%2][j - nums[i - 1]];
-                else
-                    dp[i%2][j] = dp[(i - 1)%2][j];
-            }
-        }
-        return dp[n%2][sum];
-     }
 };
