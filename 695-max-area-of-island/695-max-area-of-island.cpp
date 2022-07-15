647b037a006c02b9d5a54781552d92cdfc66bf74
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-15 14:19:04 +0530

    Time: 19 ms (90.87%), Space: 23.1 MB (89.29%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -9,27 +9,23 @@ public:
                              
                             if(grid[i][j]==1){
                                     int area=0;
-                                    dfs(grid,i,j,area);
+                                    dfs(grid,i,j,area,m,n);
                                     maxArea=max(area,maxArea);
                             }
                     }
             }
             return maxArea;
     }
-    void dfs(vector<vector<int>>& grid,int i,int j, int &area){
-            if (isValid(grid,i,j)){
-                    grid[i][j]=0;  //make it visited
-		area++;//due to i,j area increased
-           dfs(grid,i+1,j,area);
-           dfs(grid,i-1,j,area);
-	   dfs(grid,i,j+1,area);
-	   dfs(grid,i,j-1,area);
+    void dfs(vector<vector<int>>&grid,int i,int j, int &area,int m,int n){
+            if (i>=0 && j>=0 && i<m && j<n && grid[i][j]==1){
+              grid[i][j]=0;  //make it visited
+		      area++;//due to i,j area increased
+              dfs(grid,i+1,j,area,m,n);
+              dfs(grid,i-1,j,area,m,n);
+	          dfs(grid,i,j+1,area,m,n);
+	          dfs(grid,i,j-1,area,m,n);
+              }
+        
     }
-    }
-        bool isValid(vector<vector<int>>& grid,int i,int j){
-                int m=grid.size();
-                int n=grid[0].size();
-                if(i>=0 && j>=0 && i<m && j<n && grid[i][j]==1) return true;
-                else return false;
-        }
+       
 };
