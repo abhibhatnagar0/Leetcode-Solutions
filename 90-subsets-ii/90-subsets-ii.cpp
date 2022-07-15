c1752e5b733601fc3c4ac80c4b50ff49cb8679d9
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-15 20:43:05 +0530

    Time: 7 ms (49.26%), Space: 8.4 MB (33.38%) - LeetHub

--- 90-subsets-ii/90-subsets-ii.cpp
+++ 90-subsets-ii/90-subsets-ii.cpp
@@ -1,27 +1,19 @@
 class Solution {
 public:
-        vector<vector<int>> ans;
     vector<vector<int>> subsetsWithDup(vector<int>& nums) {
-        vector<int> subset;
-            sort(nums.begin(),nums.end());
-            helper(nums,0, subset);
-            return ans;
-    }
-        void helper(vector<int>& nums, int idx, vector<int>& subset){
-                if(idx==nums.size()){ ans.push_back(subset);
-                                      return; }
-                
-                subset.push_back(nums[idx]);
-                helper(nums,idx+1,subset);// including ith index
-                subset.pop_back();
-                
-                while( idx+1<nums.size() && nums[idx]==nums[idx+1]){
-                        idx++;
-                }
-                // 1,2,2,2,3
-                //when at idx=1,and we dont take it then uske aage wale 2s ko bhi nhi lena
-                helper(nums,idx+1,subset); // not including ith index   
-        }
-    
+        sort(nums.begin(),nums.end());
+        int n=nums.size(),p=1<<n; //p=total possible subsets = pow(2,n)
+        set<vector<int>> st;
         
+        for(int i=0;i<p;i++){
+            vector<int> subset;
+            for(int j=0;j<n;j++){
+                if(i&(1<<j)) subset.push_back(nums[j]);
+            }
+            st.insert(subset); //stores all unique subsets
+        }
+        
+        vector<vector<int>> res(st.begin(),st.end());
+        return res;   
+    }
 };
