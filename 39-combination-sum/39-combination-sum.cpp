0ccf1b8d2f932355cf6c149597f7160a6cc3fd6b
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-15 09:21:32 +0530

    Time: 64 ms (30.10%), Space: 19.9 MB (31.78%) - LeetHub

--- 39-combination-sum/39-combination-sum.cpp
+++ 39-combination-sum/39-combination-sum.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
-        vector<vector<int>> ans;
     vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
-        vector<int>subset;
-           int currSum=0;
-            helper(candidates,target,subset,0,currSum);
-            return ans;
-            //to make each subset sorted, phle candidate ko sort kro before passing
+            int n = candidates.size();
+        vector<vector<vector<int>>> dp(target + 1);
+    dp[0] = {{}};
+    for (int j = 0; j < n ; j++) // all combinations
+    {
+         for (int i = candidates[j]; i <= target; i++)
+        {  
+              for(vector<int> v : dp[i-candidates[j]]){
+               v.push_back(candidates[j]);
+                dp[i].push_back(v);  
+              }
+        }    
     }
-        void helper(vector<int>& candidates, int target,vector<int>& subset, int i, int &currSum){
-           if(currSum==target) {
-                        ans.push_back(subset);
-                        return;
-                }
-                if(currSum>target) return;
-                if(i==candidates.size()) return; //backtrack;
-                 
-                // taking ith idx element in subset
-                currSum+=candidates[i];
-                subset.push_back(candidates[i]);
-                helper(candidates,target,subset,i,currSum);//may think of taking i again
-                currSum-=candidates[i];
-                subset.pop_back();
-                
-                //not taking i,moving to next idx
-                helper(candidates,target,subset,i+1,currSum);
-        }
+    return dp[target];
+    }
 };
