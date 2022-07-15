86a14ee87d0f4016c060ce2fa95d13f6aac199fc
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-15 11:41:48 +0530

    Time: 699 ms (16.08%), Space: 102.9 MB (8.58%) - LeetHub

--- 474-ones-and-zeroes/474-ones-and-zeroes.cpp
+++ 474-ones-and-zeroes/474-ones-and-zeroes.cpp
@@ -1,25 +1,25 @@
 class Solution {
 public:
     int findMaxForm(vector<string>& strs, int m, int n) {
-        int s=strs.size();
-        //3 states of dp - index,m,n
-        //top down
-          vector<vector<vector<int>>> dp(s,vector<vector<int>>(m+1,vector<int>(n+1,-1)));
-           return findmax(strs,m,n,0,dp);
+        int s= strs.size();
+        vector<vector<vector<int>>>dp(s+1,vector<vector<int>>(m+1,vector<int>(n+1,0)));
+        
+        for(int i=1;i<=strs.size();i++){     
+            //considering strs[i-1]
+            int countzeroes = count(strs[i-1].begin(),strs[i-1].end(),'0');
+            int countones = strs[i-1].size()-countzeroes;
+            
+            for(int j=0;j<=m;j++) //j gives no of zeroes
+            {
+                for(int k=0;k<=n;k++) //k gives no of ones
+                {
+                    //two choices
+                    if(j-countzeroes>=0 && k-countones>=0)
+                  dp[i][j][k] = max(1+dp[i-1][j-countzeroes][k-countones], dp[i-1][j][k]);
+                    else dp[i][j][k] =  dp[i-1][j][k];   
+                }
+            }       
+        }
+        return dp[s][m][n];
     }
-    //m zeroes, n ones
-     int findmax (vector<string>& strs, int m, int n,int index,vector<vector<vector<int>>>&dp)
-     {
-         if(index==strs.size())return 0;
-         if(dp[index][m][n]!=-1)return dp[index][m][n];
-         
-         int countzeroes = count(strs[index].begin(),strs[index].end(),'0');
-         int countones = strs[index].size()-countzeroes;
-         
-         if(m-countzeroes>=0 && n-countones>=0) dp[index][m][n]= max(1+findmax(strs,m-countzeroes,n-countones,index+1,dp), 
-         findmax(strs,m,n,index+1,dp)); //inc and exc
-         else dp[index][m][n]=findmax(strs,m,n,index+1,dp);
-         
-         return dp[index][m][n];     
-     }
 };
