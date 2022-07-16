0142f528069b87621676e3f231177ea0460d7b3d
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-16 11:57:17 +0530

    Time: 317 ms (73.42%), Space: 11.6 MB (99.81%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     //same as longest common substring
-    int dp[1010][1010];
+    int dp[2][1010];
     
     //dp[i][j] tells len of common subarray (at suffix) of nums1 and nums2 when
     //only till i-1 and j-1 indexes of nums1 and nums2 are included
@@ -16,8 +16,11 @@ public:
         for(int i=1;i<=m;i++){
             for(int j=1;j<=n;j++){
                 if(nums1[i-1]==nums2[j-1]){
-                    dp[i][j]=1+dp[i-1][j-1];
-                    ans=max(ans,dp[i][j]);
+                    dp[i&1][j]=1+dp[(i-1)&1][j-1];
+                    ans=max(ans,dp[i&1][j]);
+                }
+                else{
+                    dp[i&1][j]=0;
                 }
             }
         }
