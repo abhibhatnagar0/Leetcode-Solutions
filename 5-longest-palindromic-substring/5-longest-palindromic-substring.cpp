98c28ea17da27e8bf5b6369d85e7773a44689c4f
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-16 11:19:06 +0530

    Time: 557 ms (25.29%), Space: 11.5 MB (58.60%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.cpp
@@ -1,18 +1,15 @@
 class Solution {
 public:
         int dp[1010][1010];
+    //dp[i][j] tells if substring from i to j is palindromic substring or not
     string longestPalindrome(string s) {
             int start=0;
-            int length=0; // end-start+1
+            int length= INT_MIN; // end-start+1
             memset(dp,0,sizeof(dp));
             int n=s.size(); //5
             int len=1;
            for(int diff=0; diff<n; diff++){ // 0,4 so diff upto 4
-                    for(int i=0,j=diff ; j<n ; i++,j++){                   //00 01 02 03 04
-                                                                           //11 12 13 14
-                                                                           //22 23 24
-                                                                           //33 34
-                                                                           //44   
+                    for(int i=0,j=diff ; j<n ; i++,j++){                    
                             if(diff==0) //n=1 
                                     dp[i][j]=1;
                             else if (diff==1) //n=2  
@@ -25,18 +22,16 @@ public:
                     if(s[i] == s[j] && dp[i+1][j-1]==1)dp[i][j]=1;
                     else dp[i][j]=0;
                 } 
+                        
                             if(dp[i][j]==1){
                                    if(diff+1>=length){
                                            length=diff+1;
                                            start=i;
                                    }
                             }
-            }  
+            }
            }
             string ans = s.substr(start,length);
             return ans;
-            
-            
-        
     }
 };
