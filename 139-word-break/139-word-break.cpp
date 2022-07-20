f7790bd51aa843975d75c5f367c86d0bfe26d04c
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-20 12:15:43 +0530

    Time: 34 ms (29.56%), Space: 13.3 MB (41.56%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -3,21 +3,28 @@ public:
     bool wordBreak(string s, vector<string>& wordDict) {
          unordered_set<string> dict;
         for(auto w: wordDict) dict.insert(w);
-        unordered_map<string,bool> dp;
-        return solve(s, dict, dp);
-    }
-    bool solve(string s, unordered_set<string>& dict, unordered_map<string,bool>&dp)     {
-         if(s == "") return true;
         
-        if(dp.find(s)!=dp.end()) return dp[s];
-       
-            int n = s.size();
-            for(int i = 1; i<= n; ++i ){
-                string t = s.substr(0, i);
-                if(dict.find(t) != dict.end()){
-                    if(solve(s.substr(i), dict,dp)) return dp[s]=true;
+         int n = s.size();
+        
+        vector<bool> dp(n+1, false);
+        //dp[i] stores true if prefix substring of s of length i can be
+        //segmented
+        dp[0] = true;
+        for(int i = 1; i <= n; i++)
+        {
+            for(int j = 0; j < i; j++)
+            {
+                if(dp[j]==true)
+                {
+                    string str = s.substr(j, i-j); //remaining substr
+                    if(dict.find(str)!= dict.end())
+                    {
+                        dp[i] = true;
+                        break; //move out of j loop
+                    }
                 }
-            }   
-        return dp[s]= false;
-        }
+            }
+        }     
+        return dp[n];
+    }
 };
