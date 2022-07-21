ba3caebedc6fc9e8dd0a7115cf67a16f3feb1f7a
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-07-21 20:48:03 +0530

    Time: 251 ms (42.10%), Space: 95.5 MB (50.86%) - LeetHub

--- 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -1,23 +1,30 @@
 class Solution {
 public:
     int minCost(string colors, vector<int>& neededTime) {
-        int n= colors.size();
-        int cost=0;
-        int lastcoloridx=0;
-        priority_queue<int,vector<int>,greater<int>> pq;
-        pq.push(neededTime[0]);
-        for(int i=1;i<n;i++){
-            if(colors[i]==colors[lastcoloridx]){
-                pq.push(neededTime[i]);
-                cost+=pq.top();
-                pq.pop();
+         int mincost=0,n=colors.size();
+        if(n==1) return 0;
+        
+        int maxval=neededTime[0];
+        for(int i=1;i<n;i++)
+        {
+            if(colors[i]==colors[i-1])
+            {
+                if(maxval>neededTime[i])
+                {
+                    mincost+=neededTime[i];
+                }
+                else
+                {
+                    mincost+=maxval;
+                    maxval=neededTime[i];
+                }
+                    
             }
-            else{
-                lastcoloridx=i;
-                while(!pq.empty()) pq.pop();
-                pq.push(neededTime[i]);
+            else
+            {
+                maxval=neededTime[i];
             }
         }
-       return cost; 
+        return mincost;
     }
 };
