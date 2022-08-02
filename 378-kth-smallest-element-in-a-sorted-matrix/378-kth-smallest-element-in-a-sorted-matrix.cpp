6829427b77940ef2807d337a9cae76af02203190
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-02 19:27:27 +0530

    Time: 51 ms (56.64%), Space: 13.2 MB (77.54%) - LeetHub

--- 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
+++ 378-kth-smallest-element-in-a-sorted-matrix/378-kth-smallest-element-in-a-sorted-matrix.cpp
@@ -5,13 +5,13 @@ public:
         int s=matrix[0][0];
         int e=matrix[n-1][n-1];
         int mid;
-        int count; //stores no of values in matrix smaller than mid
+        int count; //stores no of values in matrix smaller than or eq to mid
         while(s<e)
         {
             count=0;
             mid = s+(e-s)/2;
             for(int i=0;i<n;i++){
-                //checking elements smaller than mid in each row
+                //checking elements smaller than or eq to mid in each row
              count += upper_bound(matrix[i].begin() , matrix[i].end(),mid) - matrix[i].begin(); 
             }
                 if(count<k) s =mid+1;
@@ -19,4 +19,10 @@ public:
          }
         return s;
     }
+ //    matrix = [[1, 5, 9],
+ //              [10,11,13],
+ //              [12,13,15]],
+ //    1,5,9,10,11,12,13,13,15
+ // K= 1,2,3,4 ,5 ,6 ,7 ,8 ,9 ....Kth smallest
+    
 };
