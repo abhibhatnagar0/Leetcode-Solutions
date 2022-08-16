adcf4f15b4cb398a97fa7aafd22ad4ed62dddabb
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-17 00:17:35 +0530

    Time: 391 ms (23.91%), Space: 90.4 MB (30.59%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,22 +1,25 @@
 class Solution {
 public:
-    int numberOfSubarrays(vector<int>& nums, int k) {
-        return numofSubarraysWithAtmost(nums,k)- numofSubarraysWithAtmost(nums,k-1);
+    int numberOfSubarrays(vector<int>&nums, int k) {
+        int n= nums.size();
+        for(auto & x:nums){
+            if(x&1) x=1;
+            else x=0;
+        }
+        //now, subarrays with k odd numbers is same as
+        //subarrays with sum k
+        unordered_map<int,int>mp;
+        int sum=0,cnt=0;
+        int i=0;
+        while(i<n){
+            sum+=nums[i];
+            if(sum==k) cnt++;
+            if(mp.find(sum-k)!=mp.end()){
+                cnt+=mp[sum-k];
+            }
+            mp[sum]++;
+            i++;
+        }
+        return cnt;
     }
-    
-    int numofSubarraysWithAtmost(vector<int>& nums, int k){
-		int i = 0, j = 0, odds = 0, res = 0;
-
-		while(j < nums.size()){
-			if(nums[j] & 1)	odds++;
-           
-				while(odds > k){
-					if(nums[i] & 1) odds--;
-					i++;
-				}
-        	res += j-i+1;
-            j++;
-		}
-		return res;
-	}
 };
