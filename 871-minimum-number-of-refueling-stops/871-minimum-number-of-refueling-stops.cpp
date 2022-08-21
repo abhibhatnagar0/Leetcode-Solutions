6ad07855fd1d5844eb04b4631c219d6fd72a98f2
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-21 20:24:25 +0530

    Time: 45 ms (58.63%), Space: 16.3 MB (51.25%) - LeetHub

--- 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
+++ 871-minimum-number-of-refueling-stops/871-minimum-number-of-refueling-stops.cpp
@@ -1,20 +1,23 @@
 class Solution {
 public:
     int minRefuelStops(int target, int startFuel, vector<vector<int>>& stations) {
-        int count=0, i=0, curr_fuel=startFuel;
-        priority_queue<int> pq;
+        int stops=0, i=0, curr_fuel=startFuel;
+        //curr_fuel gives current reach
         
+        priority_queue<int> pq; //max heap
+        //always fill tank with max fuel available so far
+        //pq stores all previous stations fuels, where I can go with curr_fuel
+        //when I reach a station where I cannot go with curr_fuel, use pq.top() to refill tank
+        //if curr_fuel exceeds target, stop there and return stops
         while(curr_fuel < target){
-            while(i<stations.size() && curr_fuel>=stations[i][0])
-                pq.push(stations[i++][1]);
+            while(i<stations.size() && curr_fuel>=stations[i][0]) pq.push(stations[i++][1]);
             
-            if(pq.empty())
-                return -1;
-            
+            //curr_fuel < stations[i][0] ....cannot reach this station
+            if(pq.empty()) return -1; //cannot reach end, as no stations before to refill tank
             curr_fuel += pq.top();
             pq.pop();
-            count++;
+            stops++;
         }
-        return count;
+        return stops;
     }
 };
