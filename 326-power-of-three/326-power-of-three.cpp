c602ce97e0a48fb643df2edd15753dd6a38a4403
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-22 11:43:43 +0530

    Time: 28 ms (68.40%), Space: 5.8 MB (88.51%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -1,9 +1,7 @@
 class Solution {
 public:
     bool isPowerOfThree(int n) {
-        if(n<1) return false;
-        return floor(log10(n)/log10(3))==ceil(log10(n)/log10(3));
-        // floor(x) : rounds downs the nearest integer.
-        //  ceil(x) : rounds up the nearest integer.
+        if(n <= 1) return n == 1;
+	return n % 3 == 0 && isPowerOfThree(n / 3);
     }
 };
