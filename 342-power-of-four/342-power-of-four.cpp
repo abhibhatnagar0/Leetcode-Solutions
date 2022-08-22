52c367f7078de6ad3d49ff7f9efc3563be8a5c15
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-22 10:37:39 +0530

    Time: 0 ms (100.00%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,9 +1,18 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {            
+        // if(n<1) return false;
+        // else if(n==1) return true;
+        // else if(n%4==0) return isPowerOfFour(n/4);
+        // else return false;
+        
         if(n<1) return false;
-        else if(n==1) return true;
-        else if(n%4==0) return isPowerOfFour(n/4);
-        else return false;
+        return floor(logn(n,4))==ceil(logn(n,4));
+        // floor(x) : rounds downs the nearest integer.
+        //  ceil(x) : rounds up the nearest integer.
+        
+    }
+    double logn(int n,int r){
+        return log(n)/log(r);
     }
 };
