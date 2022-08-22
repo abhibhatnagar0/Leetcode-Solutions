8725c1ff96f08655ee3b9ccd4bc2dcd9d25bf8b9
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-22 12:09:26 +0530

    Time: 30 ms (30.50%), Space: 21.4 MB (31.14%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.cpp
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* lcaDeepestLeaves(TreeNode* root) {
+         if(root==NULL) return root;
+        TreeNode*p=NULL, *q=NULL;
+        //p,q are extreme nodes of deepest level in tree
+        queue<TreeNode*> qu;
+        qu.push(root);
+        while(!qu.empty()){
+            int n= qu.size();
+            for(int i=0;i<n;i++){
+                TreeNode* node= qu.front();
+                qu.pop();
+                if(i==0) p= node;
+                if(i==n-1) q= node;
+                if(node->left) qu.push(node->left);
+                if(node->right) qu.push(node->right);
+            }
+        }
+        return lca(root,p,q);
+    }
+     TreeNode* lca(TreeNode* root,TreeNode* p, TreeNode* q){
+        if(root==NULL || root==p || root==q) return root;
+        if(p==q) return p; //only one deepest node
+        
+        TreeNode* left=lca(root->left,p,q);
+        TreeNode* right=lca(root->right,p,q);
+        if(right==NULL) return left;
+        else if(left==NULL) return right;
+        else return root;
+    }
+};
