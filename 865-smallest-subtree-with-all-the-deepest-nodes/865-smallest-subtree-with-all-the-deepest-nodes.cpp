bc9ad363d7cb01724b6e05d1cd0c7abf10b0117e
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-22 12:30:56 +0530

    Time: 12 ms (28.26%), Space: 14.5 MB (25.83%) - LeetHub

--- 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
+++ 865-smallest-subtree-with-all-the-deepest-nodes/865-smallest-subtree-with-all-the-deepest-nodes.cpp
@@ -12,33 +12,46 @@
 class Solution {
 public:
     TreeNode* subtreeWithAllDeepest(TreeNode* root) {
-        if(root==NULL) return root;
-        TreeNode*p=NULL, *q=NULL;
-        //p,q are extreme nodes of deepest level in tree
-        queue<TreeNode*> qu;
-        qu.push(root);
-        while(!qu.empty()){
-            int n= qu.size();
-            for(int i=0;i<n;i++){
-                TreeNode* node= qu.front();
-                qu.pop();
-                if(i==0) p= node;
-                if(i==n-1) q= node;
-                if(node->left) qu.push(node->left);
-                if(node->right) qu.push(node->right);
-            }
-        }
-        return lca(root,p,q);
-    }
-   
-    TreeNode* lca(TreeNode* root,TreeNode* p, TreeNode* q){
-        if(root==NULL || root==p || root==q) return root;
-        if(p==q) return p; //only one deepest node
+         if(root==NULL) return root;
+         int leftdepth= getDepth(root->left);
+         int rightdepth= getDepth(root->right);
         
-        TreeNode* left=lca(root->left,p,q);
-        TreeNode* right=lca(root->right,p,q);
-        if(right==NULL) return left;
-        else if(left==NULL) return right;
-        else return root;
+         if(leftdepth > rightdepth) return subtreeWithAllDeepest(root->left);
+         else if (leftdepth < rightdepth) return subtreeWithAllDeepest(root->right);
+          return root; //when leftdepth == rightdepth
     }
+    int getDepth(TreeNode* root){
+        if(root==NULL) return 0;
+        return max(getDepth(root->left),getDepth(root->right))+1;
+    }
+//      TreeNode* subtreeWithAllDeepest(TreeNode* root) {
+//         if(root==NULL) return root;
+//         TreeNode*p=NULL, *q=NULL;
+//         //p,q are extreme nodes of deepest level in tree
+//         queue<TreeNode*> qu;
+//         qu.push(root);
+//         while(!qu.empty()){
+//             int n= qu.size();
+//             for(int i=0;i<n;i++){
+//                 TreeNode* node= qu.front();
+//                 qu.pop();
+//                 if(i==0) p= node;
+//                 if(i==n-1) q= node;
+//                 if(node->left) qu.push(node->left);
+//                 if(node->right) qu.push(node->right);
+//             }
+//         }
+//         return lca(root,p,q);
+//     }
+   
+//     TreeNode* lca(TreeNode* root,TreeNode* p, TreeNode* q){
+//         if(root==NULL || root==p || root==q) return root;
+//         if(p==q) return p; //only one deepest node
+        
+//         TreeNode* left=lca(root->left,p,q);
+//         TreeNode* right=lca(root->right,p,q);
+//         if(right==NULL) return left;
+//         else if(left==NULL) return right;
+//         else return root;
+//     }
 };
