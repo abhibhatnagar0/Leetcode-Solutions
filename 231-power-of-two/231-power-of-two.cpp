b6f0138313e264ac625dad530e197b03691cc744
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-22 11:54:34 +0530

    Time: 3 ms (59.97%), Space: 5.9 MB (70.35%) - LeetHub

--- 231-power-of-two/231-power-of-two.cpp
+++ 231-power-of-two/231-power-of-two.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPowerOfTwo(int n) {
        if(n<=1) return n==1;
-        return (n&(n-1))==0;
+        // return (n&(n-1))==0;
+        return __builtin_popcount(n)==1;
     }
 };
