6c4a3c3df7edfdc37d14a1ad80c361016fce18f6
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-25 17:13:34 +0530

    Time: 234 ms (32.41%), Space: 93.4 MB (52.40%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int maxProfit(vector<int>& prices) {
         int n= prices.size();
-        vector<int> dp(n,0);
-        //dp[i] max profit earned on ith day...ie sold on ith day
-        dp[0]=0;
         int minBefore=INT_MAX; //buying cost
         int profit=0;
         for(int i=1;i<n;i++){
