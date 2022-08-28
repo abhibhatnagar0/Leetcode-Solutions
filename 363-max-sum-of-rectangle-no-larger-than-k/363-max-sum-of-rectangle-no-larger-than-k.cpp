84f75a51ff6b0321ec4c99363697b910c8630749
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-28 13:09:45 +0530

    Time: 2003 ms (33.72%), Space: 294 MB (33.90%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -1,48 +1,29 @@
 class Solution {
 public:
-    // function for finding maximum subarray having sum less than k
-    
-    int find_max(vector<int>& arr, int k){
-        int n = arr.size();
-        int maxi = INT_MIN;
-        // curr_sum will store cumulative sum
-        int curr_sum = 0;
-        // set will store the prefix sum of array
-        set<int> s;
-        // put 0 into set, if curr_sum == k, (curr_sum - k) will be zero 
-        s.insert(0);
-        for(int i = 0; i < n; i++){
-            // calculate cumulative sum
-            curr_sum += arr[i];
-            // find the prefix sum in set having sum == curr_sum - k
-            auto it = s.lower_bound(curr_sum - k);
-            // if prefix sum is present, update the maxi
-            if(it != s.end()) maxi = max(maxi, curr_sum - *it);
-
-            // insert prefix sum into set
-            s.insert(curr_sum);
-        }
-        return maxi;
-    }
-    
-    int maxSumSubmatrix(vector<vector<int>>& matrix, int k) {
+    int maxSumSubmatrix(vector<vector<int>>& matrix, int t) {
+         int res = INT_MIN;
         int n = matrix.size();
         int m = matrix[0].size();
-        int maxi = INT_MIN;
-        // fix the position of two rows and take cumulative sum of columns between two fixed rows
-        for(int start_row = 0; start_row < n; start_row++){
-            vector<int> col_array(m, 0);
-            for(int end_row = start_row; end_row < n; end_row++){
-                // take cumulative sum of columns between two fixed rows
-                for(int col = 0; col < m; col++){
-                    col_array[col] += matrix[end_row][col];
+        for(int c1=0; c1<m; c1++){
+            vector<int>temp(n);// stores prefix sum of all rows from [c1,c2]
+            for(int c2=c1; c2<m; c2++){
+                //fixed 2 columns
+                for(int k=0; k<n; k++){
+                    temp[k] += matrix[k][c2];
                 }
-                // find maximum subarray having sum less than equal to k
-                int curr_max = find_max(col_array, k);
-                // update the maximum sum
-                maxi = max(maxi, curr_max);
+                //find maximum sum subarray in temp with sum<=k 
+                set<int>s = {0};//stores prefix sum of temp
+                int sum = 0;
+                for(auto p: temp){
+                    sum += p;
+                    auto it = s.lower_bound(sum-t); //gives iterator of sum eq to or just greater than (sum-t)
+                    if(it != s.end()){
+                        res = max(res,sum-*it);//res has sum eq to or just less than t
+                    }
+                    s.insert(sum);
+                }
             }
         }
-        return maxi;
+        return res;
     }
 };
