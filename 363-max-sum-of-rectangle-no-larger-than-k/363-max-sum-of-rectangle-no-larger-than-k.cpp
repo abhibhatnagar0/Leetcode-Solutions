07addde0c12f8cda562f0bc685b9b72d94141b8b
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-28 13:16:48 +0530

    Time: 1860 ms (46.70%), Space: 293.4 MB (42.83%) - LeetHub

--- 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
+++ 363-max-sum-of-rectangle-no-larger-than-k/363-max-sum-of-rectangle-no-larger-than-k.cpp
@@ -4,18 +4,20 @@ public:
          int res = INT_MIN;
         int n = matrix.size();
         int m = matrix[0].size();
+        // prefix sum of every row
+        for(int i=0;i<n;i++){
+            for(int j=1;j<m;j++)
+                matrix[i][j]+=matrix[i][j-1];
+        }
         for(int c1=0; c1<m; c1++){
-            vector<int>temp(n);// stores prefix sum of all rows from [c1,c2]
             for(int c2=c1; c2<m; c2++){
                 //fixed 2 columns
-                for(int k=0; k<n; k++){
-                    temp[k] += matrix[k][c2];
-                }
-                //find maximum sum subarray in temp with sum<=k 
-                set<int>s = {0};//stores prefix sum of temp
+                //find maximum sum subarray with sum<=k 
+                set<int>s = {0};
                 int sum = 0;
-                for(auto p: temp){
-                    sum += p;
+                for(int k=0; k<n; k++){
+                    sum += matrix[k][c2];
+                    if(c1>0) sum-= matrix[k][c1-1];
                     auto it = s.lower_bound(sum-t); //gives iterator of sum eq to or just greater than (sum-t)
                     if(it != s.end()){
                         res = max(res,sum-*it);//res has sum eq to or just less than t
