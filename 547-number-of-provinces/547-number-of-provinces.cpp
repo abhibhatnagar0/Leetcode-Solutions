7bfcea1107d0dffcede9e188519f9d9dc829e49f
Abhi Bhatnagar <77234929+abhibhatnagar0@users.noreply.github.com>
2022-08-29 15:46:57 +0530

    Time: 23 ms (94.10%), Space: 13.7 MB (66.18%) - LeetHub

--- 547-number-of-provinces/547-number-of-provinces.cpp
+++ 547-number-of-provinces/547-number-of-provinces.cpp
@@ -1,37 +1,44 @@
 class Solution {
 public:
-    vector<int>par;
-    vector<int>rank;
-    int find(int u)
-        {
-            if(u == par[u]) return u;
-            return par[u] = find(par[u]);
-        }
-    void unionByRank(int u, int v){
-        u=find(u);
-        v=find(v);
-        if(u==v) return;
-        if(rank[u]>rank[v]) par[v]=u;
-        else if(rank[u]<rank[v])par[u]=v;
-        else{
-            par[v]=u;
-            rank[u]++;
-        }
-    }
-    int findCircleNum(vector<vector<int>>& isConnected) {
+    // vector<int>par;
+    // vector<int>rank;
+    // int find(int u)
+    //     {
+    //         if(u == par[u]) return u;
+    //         return par[u] = find(par[u]);
+    //     }
+    // void unionByRank(int u, int v){
+    //     u=find(u);
+    //     v=find(v);
+    //     if(u==v) return;
+    //     if(rank[u]>rank[v]) par[v]=u;
+    //     else if(rank[u]<rank[v])par[u]=v;
+    //     else{
+    //         par[v]=u;
+    //         rank[u]++;
+    //     }
+    // }
+  
+       vector<int> visited;
+    int findCircleNum(vector<vector<int>>& isConnected){
         int n=isConnected.size();
-        par.resize(n);
-        rank.resize(n,0);
-        for(int i=0;i<n;i++) par[i]=i;
-        for(int i=0;i<n-1;i++){
-            for(int j=i+1;j<n;j++){
-                if(isConnected[i][j]==1) unionByRank(i,j);
+        visited.resize(n,0);
+        int cnt=0;
+       for(int i=0;i<n;i++){           
+           if(visited[i]==0) {
+               dfs(i,isConnected);
+               cnt++;
+           }
+       } 
+        return cnt;   
+    }
+    
+    void dfs(int s,vector<vector<int>>& isConnected){
+        visited[s]=1;
+        for(int i=0;i<isConnected.size();i++){
+            if(visited[i]==0 && isConnected[s][i]==1){
+                dfs(i,isConnected);
             }
         }
-        int cnt=0;
-        for(int i=0;i<n;i++){
-            if(par[i]==i) cnt++;
-        }
-        return cnt;
     }
 };
